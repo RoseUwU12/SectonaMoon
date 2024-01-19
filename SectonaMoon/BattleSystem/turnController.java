6f9e317a1fdac15e2bf89dc0dd8b0ad822a3c316
Rosemary Moon <122943402+RoseUwU12@users.noreply.github.com>
2024-01-19 12:02:10 -0500

    Update turnController.java

--- SectonaMoon/BattleSystem/turnController.java
+++ SectonaMoon/BattleSystem/turnController.java
@@ -8,5 +8,8 @@ public class turnController {
         battleSystem.playerTurn(file.import.Player(".txt") , enemy.encounterHandle(file.import.Enemy(".txt"));
         battleSystem.execute.winCondition(file.import.Encounter(".txt"));
         execute.turn(import.damageValues());
+         battleSystem.EnemyTurn(file.import.EnemyStats(enemy.encounterHandle(file.import.Enemy(".txt"))); 
+        battleSystem.repeat.victoryCondition(file.import.winConditions(".txt");
+        battleSystem.handleVictory(file.import.victoryfanfare(".mid"); 
     }
 }
