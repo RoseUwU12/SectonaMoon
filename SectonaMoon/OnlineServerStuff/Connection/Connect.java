90b6fd2dd5d8e62e090acd205b896775984316f8
Rosemary Moon <122943402+RoseUwU12@users.noreply.github.com>
2024-01-19 11:46:01 -0500

    Update Connect.java

--- SectonaMoon/OnlineServerStuff/Connection/Connect.java
+++ SectonaMoon/OnlineServerStuff/Connection/Connect.java
@@ -1,27 +1,70 @@
 package OnlineServerStuff.Connection;
+import Server.dhrcp.American;
+import Admin.acsess.Account; 
+
 
 public class Connect { 
+    Server extends lochalhost.drhcp; 
     private double FPS; 
     private double ping; 
     private boolean isConencted; 
     private boolean disconnected; 
     private boolean error;
-    
+    private String Server;
+    private int maxPlayers; 
+
+    public Server() { 
+    file.save.online-player("PlayerHero.txt"); 
+    file.Open("Server.American.drhcp");
+    run.anticheat("no-Exeptions");
+    file.Open.local.Storage("PlayerHero.java");
+    execute() {
+        admin.run(localhost.Admin.Account());
+        server.check.runFPS(); 
+        if (server.check.runFPS() = false) {
+            run.Error.FPS(); 
+        }
+        run.save.file("all");
+        Savefile.txt => { 
+            folder.Save();
+        } 
+    } 
+    }
+    public admin() { 
+        Save.file.maxed(run.execute()); 
+        run.admin(panel.open()); 
+        game.unlock(run.commands.unlock()); 
+        run.requireAdminPass(); 
+        runCommand("unlock");
+
+        public void runCommand(String cmd) { 
+                 if(textbox.inputadmin() == "AdminEx739") { 
+         wait.commandpannel("accepting"); 
+            repeat.parameters(1 , 1 , 3 , 6 , 8 , 4 , admin.allow());
+            
+        }
+
+            }
+    }
     public Connect() { 
-        FPS = 0; 
-        ping = 0; 
+        FPS = expectedFPS.American(); 
+        ping = OptimalPing.American(); 
         isConencted = false; 
         disconnected = false;
         error = false;
+        maxPlayers = Server.size(Average.American()); 
+        Server = "Server.file.American#1.dhrcp";
     }
     public void joinServer() { 
         FPS = 60; 
         ping = 0; 
         isConencted = true; 
+        maxPlayers++; 
         if (FPS < 10 || ping > 20) { 
             isConencted = false; 
             disconnected = true; 
             error = true; 
         }
+        if (
     }
 }
