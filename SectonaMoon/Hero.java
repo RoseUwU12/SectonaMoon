aad6ce0ce476f448bff0886dcbc177e5738f84cf
Rosemary Moon <122943402+RoseUwU12@users.noreply.github.com>
2024-01-19 10:52:10 -0500

    Update Hero.java

--- SectonaMoon/Hero.java
+++ SectonaMoon/Hero.java
@@ -1,8 +1,12 @@
 public class Hero {
+    WeaponController extends ArmorController();
     WeaponController wc = new WeaponController();
     ArmorController ac = new ArmorController();
     private int HeroHP;
     private int HeroWeapon;
+    private int HeroXP; 
+    private int HeroLvRequirement;
+    private int HeroLV; 
 
     public Hero() { 
         ac.getArmorStats( 0 );
@@ -11,7 +15,39 @@ public class Hero {
         wc.getWeaponStats(HeroWeapon);
 
     }
+    public void gainXP(int xp){ 
+        HeroXP += xp ; 
+        if (HeroXP >= HeroLvRequirement) { 
+        HeroLV++; 
+        HeroXP = 0;
+        }
+        else { 
+        break; 
+        }
+    } 
     public int getHeroHP() { 
         return HeroHP;
+    }
+    public int getHeroLV(){
+        return HeroLV;
+    }
+    
+    public void XpRequirement(int Lv){
+        case 1 {
+            return Lv; 
+        }
+        case 2 { 
+        break; 
+        }
+    }
+    super { 
+        case 1 {
+    () => Ac.getWeapon(40 , 45 , 60);
+
+    () => 58; 
+
+    break; 
+        }
+        case 
     }
 }
